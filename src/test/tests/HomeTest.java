4949cfdc8526206ff62cb883d94c3f5fd5b163fc
kwanruethai sorntem <ksorntem@gmail.com>
2022-06-04 10:01:08 -0400

    create template

---
+++ src/test/tests/HomeTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utils.SeleniumUtils;
+
+public class HomeTest extends BaseTest {
+    HomePage homePage;
+
+    @BeforeMethod
+    public void localSetUp() {
+
+        homePage = new HomePage(getDriver());
+    }
+}
+
