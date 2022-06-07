c17b85744e184a46000ad1cc34881ceed010cea7
Pilasinee Tirathiensakul <nuitoei2@hotmail.com>
2022-06-06 20:28:46 -0400

    Add AUT-6 Recently Viewed Items

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -6,12 +6,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class HomePage extends BasePage {
+
     protected WebDriver driver;
 
-    public HomePage(WebDriver driver) {
-        super(driver);
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
+        public HomePage(WebDriver driver){
+            super(driver);
+            this.driver = driver;
+            PageFactory.initElements(driver, this);
+        }
+    @FindBy(xpath = "//a[@class='product-name' and @title='Faded Short Sleeve T-shirts']")
+    public WebElement Shirt;
+
+    @FindBy (xpath = "//a[@class='fancybox-item fancybox-close']")
+    public WebElement closeBtn;
+
+    @FindBy (linkText = "Quick view")
+    public WebElement quickViewBtn;
+
+    @FindBy (xpath = "(//a[text()='T-shirts'])[2]")
+    public WebElement TshirtBtn;
+
+    public String expectedShirtItemText = "Faded Short Sleeve T-shirts";
+
+
+
+
     }
-}
+
+
+
---
+++ src/test/tests/QuickViewTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.QuickViewPage;
+
+public class QuickViewTest extends BaseTest {
+    HomePage homePage;
+    QuickViewPage quickViewPage;
+
+    @BeforeMethod
+    public void localSetup() {
+        quickViewPage = new QuickViewPage(getDriver());
+        homePage = new HomePage(getDriver());
+
+    }
+
+    @Test(testName = "AUT-6 Recently viewed items", description = "should see dresses displayed in Viewed Products section")
+    public void testQuickView() {
+
+        homePage.moveIntoView (homePage.Shirt);
+
+        homePage.quickViewBtn.click();
+
+
+
+            }
+        }
+
+
+
+
+
